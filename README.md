830eaef36ccc9fa0557b1690e2ea894a25b3340b
Peter Kordel <pkordel@gmail.com>
2011-04-07 13:54:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Testing with RSpec, Watchr and Spork
+====================================
+
+1. rake db:migrate db:test:prepare
+2. new shell: rake watchr
+3. new shell: spork
+4. make changes, watch tests
+
+What where
+----------
+
+see .watchr, .rspec, spec/spec_helper
+
+[Article on RubyInside](http://www.rubyinside.com/how-to-rails-3-and-rspec-2-4336.html)_
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testing::Application.config.secret_token = '5858acd59892ac4e51167eddb3177448ea0246f95db10766219e706ec8dd1f821762509e205ce8eda18872d844205ec66cf39108038b6622e355fdf14f781c39'
